7f5712d7a1090db6be07ff852bce9d0e161461f0
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-14 00:46:18 +0300

    In style

--- props-cr/src/components/Alert.jsx
+++ props-cr/src/components/Alert.jsx
@@ -1,6 +1,14 @@
-export const Alert = ({ children }) => {
+export const Alert = ({ children , type="success"}) => {
     return (
-        <div>
+        <div 
+            style={{
+            backgroundColor: type === "success" ? "green" : "red", 
+            color:"#fff", 
+            padding:"10px",
+            borderRadius:"5px",
+            marginBottom:"20px"   
+         }}
+         >
             {children}
         </div>
     )
