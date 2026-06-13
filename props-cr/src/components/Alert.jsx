e94e8752b90ad8e9ebfb626db65606c51ea63077
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-14 02:49:08 +0300

    Changing the route path

--- props-cr/src/components/Alert.jsx
+++ props-cr/src/components/Alert.jsx
@@ -1,15 +1,29 @@
-import "../../src/Alert.css"
+// import "../../src/Alert.css"
+
+// export const Alert = ({ children , type="success"}) => {
+//     return (
+//         <div 
+//             style={{
+//             backgroundColor: type === "success" ? "green" : "red", 
+//             color:"#fff", 
+//             // padding:"10px",
+//             // borderRadius:"5px",
+//             marginBottom:"20px"   
+//          }}
+//          >
+//             {children}
+//         </div>
+//     )
+// }
+
+
+// import "../../src/Alert.css"
+import "./../Alert.css"
 
 export const Alert = ({ children , type="success"}) => {
     return (
         <div 
-            style={{
-            backgroundColor: type === "success" ? "green" : "red", 
-            color:"#fff", 
-            // padding:"10px",
-            // borderRadius:"5px",
-            marginBottom:"20px"   
-         }}
+            className={`alert ${type}`} // you call a literal  name
          >
             {children}
         </div>
