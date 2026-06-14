9692dd79adedb0ec37b076b5fe7a4b697b5af6d2
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-14 03:20:28 +0300

    changes

--- props-cr/src/components/Alert.jsx
+++ props-cr/src/components/Alert.jsx
@@ -18,14 +18,22 @@
 
 
 // import "../../src/Alert.css"
-import "./../Alert.css"
+// import "./../Alert.module.css"
 
-export const Alert = ({ children , type="success"}) => {
-    return (
-        <div 
-            className={`alert ${type}`} // you call a literal  name
-         >
-            {children}
-        </div>
-    )
+// export const Alert = ({ children , type="success"}) => {
+//     return (
+//         <div 
+//             className={`alert ${type}`} // you call a literal  name
+//          >
+//             {children}
+//         </div>
+//     )
+// }
+
+
+import styles from "./../Alert.module.css"
+
+export const Alert = ({ children, type = "success" }) => {
+  return <div className={`${styles.alert} ${styles[type]}`}>{children}</div>;
+
 }
