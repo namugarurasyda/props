82d091ef3feb225ba20d5d80c99e137779caff7e
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-16 00:01:01 +0300

    Adding a url

--- props-cr/src/components/Profile.jsx
+++ props-cr/src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export const Profile = ({ name="Syda", description, urlavatar }) => {
+export const Profile = ({ name="Syda", description, urlavatar, websiteurl}) => {
     return (
         <div>
             <h3>{name}</h3>
             <p>{description}</p>
             <img src={urlavatar} alt={name} />
-            
+            <a href={websiteurl} target="_blank" rel="noopener noreferrer">Syda</a>
+
         </div>
     );
 };
