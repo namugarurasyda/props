42bc6788f1dfc94eb263b044b7a750a187844ff7
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-15 23:43:41 +0300

    Change

---
+++ props-cr/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Profile = ({ name="Syda", description, urlavatar }) => {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <img src={urlavatar} alt={name} />
+        </div>
+    );
+};
+
+
