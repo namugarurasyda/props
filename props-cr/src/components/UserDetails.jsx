44b2b2e805bde21bdf509b9a358d57e93705ba30
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-23 16:42:33 +0300

    Completing the Product form

--- props-cr/src/components/UserDetails.jsx
+++ props-cr/src/components/UserDetails.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 export const UserDetails = ({ name, isOnline }) => {
-  
+  const [isHovered, setIsHovered] = useState(false);
 
     return (
        
         <div>
             <h3>{name}</h3>
-            <span>{isOnline ? "🟢 Online" : "Offline"}</span>
+            <span
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                {isOnline ? "🟢 Online" : "Offline"}
+            </span>
             <p>{isOnline ? "Available for chat" : "Not available"}</p>
             {isOnline ? (
                 <button>Send Message</button>
