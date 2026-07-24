eb3b3d7ab1bf9d628455e41c18472258ff4af25a
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-25 00:04:34 +0200

    Adding an increment return

--- props-cr/src/components/ProductForm.jsx
+++ props-cr/src/components/ProductForm.jsx
@@ -18,6 +18,24 @@ export default function ProductPage({ productId, referrer, theme }) {
 
 export default function App() {
   const [incrementBy, setIncrementBy] = useState(1);
+
+   return (
+    <>
+      <label>
+        Increment by:{' '}
+        <select
+          value={incrementBy}
+          onChange={(e) => setIncrementBy(Number(e.target.value))}
+        >
+          <option value={1}>1</option>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+        </select>
+      </label>
+      <hr />
+      <Counter incrementBy={incrementBy} />
+    </>
+  );
 }
 
 function post(url, data) {
@@ -25,3 +43,4 @@ function post(url, data) {
   console.log('POST /' + url);
   console.log(data);
 }
+
